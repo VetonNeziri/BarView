a6eed31bc3ea66c5ac77f577de98a81848d93960
Veton Neziri <veton.neziri@bridge-technology.net>
2022-05-11 10:01:36 +0200

    Initial commit

---
+++ app/src/main/java/com/veton/barview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.veton.barview
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.veton.bar_view.Bar
+import com.veton.barview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val bars = listOf(
+        Bar(0f, 10f, "Bar 1", Color.GREEN),
+        Bar(10f, 30f, "Bar 2", Color.RED),
+        Bar(30f, 50f, "Bar 3", Color.BLUE)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.barView.setBars(bars)
+    }
+}
---
+++ bar-view/src/main/java/com/veton/bar_view/BarViewSideStyle.java
@@ -0,0 +1,7 @@
+package com.veton.bar_view;
+
+public class BarViewSideStyle {
+    public static final int NORMAL = 0;
+    public static final int ROUNDED = 1;
+    public static final int ANGLE = 2;
+}
